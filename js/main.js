10a24de2141416ae238994ade67ae04326ebce41
Ananya Sethi <ananyasethi56@gmail.com>
2017-11-21 16:21:52 +0530

    Some changes to the validations

--- js/main.js
+++ js/main.js
@@ -102,23 +102,75 @@ var submitForm = ()=> {
         } 
 // it will edit the data by having rollno value
     if (editData) {
-        console.log(1);
-        let name = $(`#name`).val();
-        let roll = $(`#roll`).val();
-        let stream = $(`#str`).val();
-        let year = $(`#year1`).val();
-        let a = document.getElementsByTagName(`tr`);
+        let name = document.getElementById(`name`).value;
+        let roll = document.getElementById(`roll`).value;
+        let str = document.getElementById(`str`).value;
+        let year2 = document.getElementById(`year1`).value;
+        let pttrn1=/[a-z A-Z]$/;
+        let pttrn2=/^[0-9]{4}$/;
+        let pttrn3=/^[0-9]{10}$/;
+        let pttrn4=/^[a-z A-Z]{3}$/;
         
-        for(let val of a) {
-            let row = val.children[1].innerHTML;
-            if (row == document.getElementById(`roll`).value) {
-                val.childNodes[0].innerHTML = name;
-                val.childNodes[1].innerHTML = roll;
-                val.childNodes[2].innerHTML = stream;
-                val.childNodes[3].innerHTML = year;
-
-            }
+        // Conditions for validations
+        if (name == `` ||roll == ``|| str == `` || year2 == ``) {
+            alert(`ALL ENTERIES ARE REQUIRED!!!!!`);
+            return false;
         }
+            
+          else  if (roll.length != 10) {
+              
+               alert(`The Contact No. must be at least 10 digit long!`);
+               return false;
+          }
+        else if(!pttrn1.test(name))
+               {
+               alert(`ENTER NAME IN CORRECT FORMAT`);
+               return false;
+               }
+          else if(!pttrn2.test(year2))
+               {
+               alert(`ENTER YEAR IN CORRECT FORMAT`);
+               return false;
+               }
+           else if(!pttrn3.test(roll))
+               {
+               alert(`ROLLNO IN CORRECT FORMAT`);
+               return false;
+               }
+               else if(!pttrn4.test(str))
+               {
+                alert(`STREAM SHOULD BE IN CORRECT FORMAT`);
+                return false;
+               }
+
+               else {
+                    let array=document.getElementsByTagName(`tr`);
+                    let flag=1;	
+                    let i=1;
+                // it will check the dupliacy of the records!!
+                while(i<array.length)
+                {
+                    
+                    let row=array[i].children[1].innerHTML;	
+                    if(row==document.getElementById(`roll`).value)
+                    {flag=0;}
+                    i++;
+                }
+                
+                if(flag == 0)
+                {
+                    alert(`ENTER NEW RECORD`);
+                }
+               else{ 
+                   // it will append the new row in the table
+                let markup = `<tr><td>` + name + `</td><td>` + roll + `</td><td>` + str + `</td><td>`
+                + year2 + `</td><td><input type='checkbox' name='record'></td></tr>`;
+                $(`table tbody`).append(markup);
+                $(`#form1`).hide();
+                return false;
+            }
+                
+            }
         $(`#form1`).hide();
     }
 
